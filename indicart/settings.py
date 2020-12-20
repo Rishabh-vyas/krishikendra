16e1adb64e0ff7726f83c87966e5b0bd35dc7412
ItsRishuDev <54630851+ItsRishuDev@users.noreply.github.com>
2020-12-20 11:49:09 +0530

    add static

--- indicart/settings.py
+++ indicart/settings.py
@@ -122,13 +122,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_ROOT = '/staticfiles/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
 
 # Managing media
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
